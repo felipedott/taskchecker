a2d13e84b97b20ae731df4028dcab80a90151a27
Josenilton Duarte <n8zion@gmail.com>
2022-11-29 19:47:05 -0300

    after headache

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get '/dashboard', to: 'pages#dashboard', as: 'dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :teams
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
 end
