ab4eef33d9cac50eef0c3bd3428c94258cb1914d
Josenilton Duarte <n8zion@gmail.com>
2022-11-29 13:28:43 -0300

    devise and pundit ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name email])
-
-    # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name email])
   end
 
   include Pundit::Authorization
 
-  after_action :verify_authorized
-  after_action :verify_policy_scoped
+  after_action :verify_authorized, except: %i[home], unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: %i[home], unless: :skip_pundit?
 
   private
 
