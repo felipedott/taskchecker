8504a3dff2b11959294ef932e666da7f65f72d12
Felipe Dottling <felipedott@gmail.com>
2022-11-30 10:41:05 -0500

    lots of small changes

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,8 @@ class Task < ApplicationRecord
   # belongs_to :team -> as linhas abaixo ja deixam isso implicito
   belongs_to :member, class_name: 'User'
   belongs_to :manager, class_name: 'User'
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :member_id, presence: true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,5 @@ class Team < ApplicationRecord
   has_many :team_members, dependent: :destroy
   has_many :users, through: :team_members
   has_many :tasks
+  validates :name, presence: true
 end
