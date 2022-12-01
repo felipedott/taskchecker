baf6ad57038e579d66ec221a9bcfbf8cd9ec7309
tadeu tovar <tadeutovar@hotmail.com>
2022-12-01 10:20:51 -0300

    ...

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -1,9 +1,10 @@
 class TeamMembersController < ApplicationController
 
-  def join(team)
-    @user = current_user
+  def join
+    @user = User.find(params[:id])
+    @team = Team.find(params[:id])
 
-    @team_member = TeamMember.new(user: @user, team: team)
+    @team_member = TeamMember.new(user: @user, team: @team)
     @team_member.save
     # if @team_member.save
     #   redirect_to team_path(team), notice: "New team created!"
