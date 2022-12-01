76e7f45d9a85ca3d8e86db43f9769e79a56b5080
tadeu tovar <tadeutovar@hotmail.com>
2022-12-01 11:12:59 -0300

    invitation done

---
+++ app/policies/team_member_policy.rb
@@ -0,0 +1,13 @@
+class TeamMemberPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+
+  end
+
+  def create?
+    true
+  end
+end
