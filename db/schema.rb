33cce1871d13c69166100d9c8122675299b6ca0d
tadeu tovar <tadeutovar@hotmail.com>
2022-12-06 14:19:58 -0300

    subtasks

--- db/schema.rb
+++ db/schema.rb
@@ -107,8 +107,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_164337) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
     t.string "uid"
