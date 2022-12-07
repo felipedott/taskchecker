02142cf98cc09412a1e03749bb3a8734f0493214
Felipe Dottling <felipedott@gmail.com>
2022-12-07 09:09:22 -0500

    chat css

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,11 @@
 class ChatroomsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @chatroom = policy_scope(Chatroom)
+    authorize @chatroom
+  end
+
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
